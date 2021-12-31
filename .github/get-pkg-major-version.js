6818c59731d5ff7ce6f99e9379c2a2942ae666c7
nichoth <nichoth@gmail.com>
2021-12-30 23:43:12 -0800

    rm github

--- .github/get-pkg-major-version.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-// SPDX-FileCopyrightText: 2021 Andre 'Staltz' Medeiros
-//
-// SPDX-License-Identifier: Unlicense
-
-var pkg = require('../package.json');
-console.log(pkg.version.replace(/\..*/g, ''));
